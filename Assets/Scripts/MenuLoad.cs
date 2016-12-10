1d25cf97038cb5740c9280d77fe08683e3177d07
Lena <lena.held@web.de>
2016-12-10 11:26:50 +0100

    Erstes Menü
    Erste Szenen für Menü und Spiel mit ladendem Wechsel

---
+++ Assets/Scripts/MenuLoad.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuLoad : MonoBehaviour {
+
+	public GameObject loadImage;
+
+	public void Load(int id) {
+		loadImage.SetActive (true);
+		SceneManager.LoadScene (id);
+	}
+}

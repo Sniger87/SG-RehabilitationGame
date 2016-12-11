d9d935c2f876be5dd9ed1426b90b471b8257b733
saumweber <florian.saumweber@gmx.de>
2016-12-11 23:41:24 +0100

    erste Gehversuche

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+	public float speed = 100;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(45, 45, 45), speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Sync2Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sync2Player : MonoBehaviour {
+	private Rigidbody rigidbodyComponent;
+	private Vector3 movement;
+	private Vector3 buffer;
+
+		// Use this for initialization
+		void Start () {
+			rigidbodyComponent = GetComponent<Rigidbody>();
+			buffer = new Vector3 (0, 0, 0);
+			if (gameObject.GetComponent<Player> () != null) {
+				Player player = gameObject.GetComponent<Player> ();
+				buffer = player.getMovement ();
+			}
+		}
+
+		// Update is called once per frame
+		void Update() {
+			if (gameObject.GetComponent<Player> () != null) {
+				Player player = gameObject.GetComponent<Player> ();
+				buffer = player.getMovement ();
+			}
+			movement = buffer;
+		}
+
+		void FixedUpdate() {
+		// Move the game object
+		rigidbodyComponent.velocity = movement;
+		}
+	}
+
+

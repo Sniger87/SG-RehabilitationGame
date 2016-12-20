08a0944f05c9e1b868f79171d0e288b5e93cb8c7
Sniger87 <tobi.ochs@googlemail.com>
2016-12-20 17:36:14 +0100

    Hinzufügen der WiiInput.dll und anpassen der Movement.cs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,36 +1,63 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Linq;
+using UnityEngine;
+using Wii.Input.Controllers;
+using Wii.Input.DesktopFacades;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
 
-	private CharacterController controller;
-	private Vector3 moveVector;
+    private CharacterController controller;
+    private Vector3 moveVector;
+    private BalanceBoard balanceBoard;
 
-	//artificial gravity
-	private float vertVelo = 0.0f;
-	private float gravity = 12.0f;
+    //artificial gravity
+    private float vertVelo = 0.0f;
+    private float gravity = 12.0f;
 
-	public float Speed = 3.0f;
-	// Use this for initialization
-	void Start () {
-		controller = GetComponent<CharacterController> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-		moveVector = Vector3.zero;
-		
-		if (controller.isGrounded) {
-			vertVelo = -0.5f;
-		}
-		else {
-			vertVelo -= gravity * Time.deltaTime;
-		}
-		
-		moveVector.x = Input.GetAxisRaw("Horizontal") * Speed;
-		moveVector.y = vertVelo;
-		moveVector.z = Speed;
-		controller.Move (moveVector * Time.deltaTime);
-	}
+    public float Speed = 3.0f;
+    // Use this for initialization
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        try
+        {
+            balanceBoard = null; //WiiInputManager.Current.FindAllWiiControllers().FirstOrDefault() as BalanceBoard;
+        }
+        catch (System.Exception)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        moveVector = Vector3.zero;
+
+        if (controller.isGrounded)
+        {
+            vertVelo = -0.5f;
+        }
+        else
+        {
+            vertVelo -= gravity * Time.deltaTime;
+        }
+
+        if (balanceBoard != null)
+        {
+            balanceBoard.UpdateState();
+            moveVector.x = balanceBoard.WiiControllerState.BalanceBoardState.SensorValuesKg.BottomRight * Speed;
+        }
+        else
+        {
+            moveVector.x = Input.GetAxisRaw("Horizontal") * Speed;
+        }
+
+        moveVector.y = vertVelo;
+        moveVector.z = Speed;
+        controller.Move(moveVector * Time.deltaTime);
+    }
 }

3d20d8cee1c0b06c54201ee5552b201461ae4d2c
Lena <lena.held@web.de>
2016-12-20 13:28:47 +0100

    kosmetik & Zähler für Kollisionen
    - Punkte werden gezählt
    - Zusammenstöße mit Hindernissen werden gezählt
    - kleine UI
    - mehr Materials
    - Änderungen an den Prefabs

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -1,19 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Collision : MonoBehaviour {
 
+	public Text counter;
+	public Text collisions;
+
+	private int coinsCollected = 0;
+	private int collisionCounter = 0;
+
 	void OnTriggerEnter (Collider col) {
 		if (col.gameObject.tag == "Obstacle") {
-			
+			collisionCounter++;
 			Destroy (col.gameObject);
-
+			SetText ();
 		}
 
 		if (col.gameObject.tag == "Coin") {
 
+			coinsCollected++;
 			Destroy (col.gameObject);
+			SetText ();
 		}
+	}
+
+	void SetText () {
+		counter.text = "Coins: " + coinsCollected.ToString ();
+		collisions.text = "Collisions: " + collisionCounter.ToString ();
 	}
 }

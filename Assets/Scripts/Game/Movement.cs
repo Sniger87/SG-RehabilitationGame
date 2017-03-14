eb3be5341fcfb3ce715b50582a8668a5c7172f06
Sniger87 <tobi.ochs@googlemail.com>
2017-03-14 03:14:41 +0100

    ControllerState Update über Thread
    um Update des Scripts nicht zu blockieren wird das Lesen vom Controller
    in anderen Thread ausgelagert und dort auch erfasst (Update alle 20 ms)

--- Assets/Scripts/Game/Movement.cs
+++ Assets/Scripts/Game/Movement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using Profiles;
 using UnityEngine;
 
@@ -8,8 +9,11 @@ public class Movement : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 moveVector;
-    private StringBuilder stringBuilder;
+    private StringBuilder positionBuilder;
+    private StringBuilder movementBuilder;
     private Collider playerCollider;
+
+    private Thread updateThread;
 
     //artificial gravity
     private float vertVelo = 0.5f;
@@ -22,11 +26,17 @@ public class Movement : MonoBehaviour
     // Use this for initialization
     private void Start()
     {
-        this.stringBuilder = new StringBuilder();
-        this.stringBuilder.AppendLine("x;y;");
+        this.positionBuilder = new StringBuilder();
+        this.positionBuilder.AppendLine("x;y;");
+
+        this.movementBuilder = new StringBuilder();
+        this.positionBuilder.AppendLine("TopLeft;TopRight;BottomLeft;BottomRight;");
 
         controller = GetComponent<CharacterController>();
         playerCollider = GetComponent<Collider>();
+
+        this.updateThread = new Thread(UpdateBalanceBoardMovement);
+        this.updateThread.Start();
     }
 
     // Update is called once per frame
@@ -44,7 +54,6 @@ public class Movement : MonoBehaviour
 
         if (BalanceBoardManager.Current.IsBalanceBoardConnected)
         {
-            BalanceBoardManager.Current.BalanceBoard.GetUpdate();
             moveVector.x = BalanceBoardManager.Current.BalanceBoard.WiiControllerState.BalanceBoardState.SensorValuesKg.BottomRight * Speed;
         }
 
@@ -57,21 +66,50 @@ public class Movement : MonoBehaviour
         if (playerCollider != null && Time.time > nextActionTime)
         {
             nextActionTime += period;
-            AppendMovementLine(playerCollider.bounds);
+            AppendPositionLine(playerCollider.bounds);
         }
     }
 
-    private void AppendMovementLine(Bounds bounds)
+    private void AppendPositionLine(Bounds bounds)
     {
-        this.stringBuilder.AppendLine(string.Format("{0};{1}", bounds.center.x, bounds.center.z));
+        this.positionBuilder.AppendLine(string.Format("{0};{1};", bounds.center.x, bounds.center.z));
     }
 
     private void OnDestroy()
     {
-        if (this.stringBuilder != null)
+        if (this.updateThread != null)
         {
-            ProfileManager.Current.WritePlayerPositionFile(this.stringBuilder.ToString());
-            this.stringBuilder = null;
+            this.updateThread.Abort();
+            this.updateThread = null;
+        }
+
+        if (this.positionBuilder != null && this.positionBuilder.Length > 0)
+        {
+            ProfileManager.Current.WritePlayerPositionFile(this.positionBuilder.ToString());
+            this.positionBuilder = null;
+        }
+
+        if (this.movementBuilder != null && this.movementBuilder.Length > 0)
+        {
+            ProfileManager.Current.WriteMovementFile(this.movementBuilder.ToString());
+            this.movementBuilder = null;
+        }
+    }
+
+    private void UpdateBalanceBoardMovement()
+    {
+        while (true)
+        {
+            if (BalanceBoardManager.Current.IsBalanceBoardConnected)
+            {
+                BalanceBoardManager.Current.BalanceBoard.GetUpdate();
+                this.movementBuilder.AppendLine(string.Format("{0};{1};{2};{3};",
+                    BalanceBoardManager.Current.BalanceBoard.WiiControllerState.BalanceBoardState.SensorValuesRaw.TopLeft,
+                    BalanceBoardManager.Current.BalanceBoard.WiiControllerState.BalanceBoardState.SensorValuesRaw.TopRight,
+                    BalanceBoardManager.Current.BalanceBoard.WiiControllerState.BalanceBoardState.SensorValuesRaw.BottomLeft,
+                    BalanceBoardManager.Current.BalanceBoard.WiiControllerState.BalanceBoardState.SensorValuesRaw.BottomRight));
+            }
+            Thread.Sleep(20);
         }
     }
 }
